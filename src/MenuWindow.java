355e5899bd4630a030e741d5db1f29f8bb53c378
Andrii Yatsun <114068112+andriiyatsun@users.noreply.github.com>
2025-06-17 21:10:20 +0300

    Commit: Update program ralization

--- src/MenuWindow.java
+++ src/MenuWindow.java
@@ -8,10 +8,15 @@ public class MenuWindow extends JFrame {
     private static final int WINDOW_HEIGHT = 420;
     private static final int WINDOW_WIDTH = 380;
 
+    private static final int LABEL_NAME_WIDTH = 300;
+    private static final int LABEL_NAME_HEIGHT = 100;
+
     private static final int BUTTON_WIDTH = 200;
     private static final int BUTTON_HEIGHT = 60;
 
     private static final int BUTTON_FONT_SIZE = 14;
+
+    private static final int LABEL_NAME_FONT_SIZE = 30;
 
     private static final int BUTTON_VERTICAL_MARGIN = 15;
 
@@ -20,7 +25,7 @@ public class MenuWindow extends JFrame {
         ImageIcon icon = new ImageIcon("resources/images/logo.png");
         setIconImage(icon.getImage());
 
-        this.setTitle("Frog Adventures");
+        this.setTitle("PAC-MAN");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         this.setLocationRelativeTo(null);
@@ -32,6 +37,7 @@ public class MenuWindow extends JFrame {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
         /* Creating buttons */
+        JLabel game_name = createLabel("\"PAC-MAN\"");
         JButton button_play = createButton("PLAY");
         JButton button_scores = createButton("SCORES");
         JButton button_exit = createButton("EXIT");
@@ -41,9 +47,9 @@ public class MenuWindow extends JFrame {
         button_play.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new Frog();
-                setVisible(false);
                 System.out.println("Play button clicked");
+                new Pacman();
+                setVisible(false);
             }
         });
 
@@ -63,10 +69,13 @@ public class MenuWindow extends JFrame {
         panel.add(Box.createVerticalGlue());
 
         /* Adding buttons to the panel with space between them */
+        panel.add(game_name);
+        panel.add(Box.createRigidArea(new Dimension(0, BUTTON_VERTICAL_MARGIN)));
         panel.add(button_play);
         panel.add(Box.createRigidArea(new Dimension(0, BUTTON_VERTICAL_MARGIN)));
-        panel.add(button_scores);
-        panel.add(Box.createRigidArea(new Dimension(0, BUTTON_VERTICAL_MARGIN)));
+        ///game scores
+//        panel.add(button_scores);
+//        panel.add(Box.createRigidArea(new Dimension(0, BUTTON_VERTICAL_MARGIN)));
         panel.add(button_exit);
 
         /* Adding vertical glue to center buttons vertically */
@@ -98,5 +107,19 @@ public class MenuWindow extends JFrame {
         button.setMaximumSize(buttonSize);
 
         return button;
+    }
+
+    private JLabel createLabel(String text){
+        JLabel label = new JLabel(text, JLabel.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, LABEL_NAME_FONT_SIZE));
+        label.setForeground(Color.yellow);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        Dimension labelSize = new Dimension(LABEL_NAME_WIDTH, LABEL_NAME_HEIGHT);
+        label.setPreferredSize(labelSize);
+        label.setMinimumSize(labelSize);
+        label.setMaximumSize(labelSize);
+
+        return label;
     }
 }
--- src/Pacman.java
+++ src/Pacman.java
@@ -1,12 +1,12 @@
 import javax.swing.JFrame;
 
-public class Frog extends JFrame{
+public class Pacman extends JFrame{
 
-    public Frog() {
+    public Pacman() {
         add(new GameWindow());
 
         this.setVisible(true);
-        this.setTitle("Frog Adventures");
+        this.setTitle("PAC-MAN");
         this.setSize(380,425);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
